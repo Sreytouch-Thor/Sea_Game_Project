7a8c1ab8626de33ed3bbbb0466ada7fa82ef0381
SREYTOUCH.THOR <THOR.SREYTOUCH@pnc.lan>
2023-05-12 19:56:47 +0700

    create crup User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = User::all();
+        // dd(User::all());
+        return response()->json(['success' => true, 'data' => $user],201);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = User::create([
+            'name' => Request('name'),
+            'email' => Request('email'),
+            'password' => Request('password')
+        ]);
+        return response()->json(['success' => true, 'data' => $user],200);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::find($id);
+        return response()->json(['success' => true, 'data' => $user],200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::find($id);
+        $user->update([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ]);
+        return response()->json(['success' => true, 'data' =>$user],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(['success' => true, 'data' =>$user],200);
+    }
+}
